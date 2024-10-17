579d911ce1324c9a0d2283861b7a8b2aca93eeeb
Guilherme Palhano <162469283+guilhermepalhano@users.noreply.github.com>
2024-10-17 11:09:40 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,25 +1,53 @@
-// fantasia, aventura, drama
-
-// o menino que descobriu o vento, 14, drama
-// fratura, 14, aventura
-// as aventuras de pi, 10, drama, fantasia, aventura
-// o menino do pijama listrado, 12, drama, fantasia
-// animais noturnos, 16, drama, aventura
-// no ritmo da fe, 10, drama
-// o leitor, 14, drama, fantasia
+let campoIdade;
+let campoFantasia;
+let campoAventura;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+  campoAventura = createCheckbox("Gosta de aventura?");
 }
 
 function draw() {
-  background(220);
-  campoIdade = createinput("5");
-}
-
-function draw() {
-  background(220);
+  background("black");
   let idade = campoIdade.value();
-  let recomendacao = geraRecomendacao(idade);
-  text(recomendacao,)
+  let gostaDeFantasia = campoFantasia.checked();
+  let gostaDeAventura = campoAventura.checked();
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
+
+  fill(color(76, 0, 115));
+  textAlign(CENTER, CENTER);
+  textSize(38);
+  text(recomendacao, width / 2, height / 2);
+}
+
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "O menino que descobriu o vento";
+    } else {
+      if (idade >= 12) {
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "scooby-doo - o misterio começa";          
+        } else{
+         return "Vingadores";
+        }
+      } else {
+        if (gostaDeFantasia) {
+          return "Planeta dos macacos";
+        } else {
+          return "velozes e furiosos";
+        }
+      }
+    }
+  } else {
+    if (gostaDeFantasia) {
+      return "Homem aranha: no aranha verso";
+    } else {
+      return "A ilha da fantasia";
+    }
+  }
 }
